4118027e29ac86ba90fafcda515de52a06201750
Karan gupta <143303605+karangupta982@users.noreply.github.com>
2024-11-04 23:34:06 +0530

    Update Razorpay.js
    UPDATE ids

--- Configuration/Razorpay.js
+++ Configuration/Razorpay.js
@@ -1,6 +1,6 @@
 const Razorpay = require("razorpay");
 
 exports.instance = new Razorpay({
-	key_id: "rzp_test_t4LUM04KXw6wHc",
-	key_secret: "DOdtPrjZRxQejIdj1vAzm0MY",
+	key_id: process.env.RAZORPAY_KEY,
+	key_secret: "RAZORPAY_SECRET",
 });
