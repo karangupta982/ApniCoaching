5ac1d631d64ef63e907abcf058b1232e5ccc9ef7
Karan gupta <143303605+karangupta982@users.noreply.github.com>
2024-11-05 08:29:04 +0530

    Update Razorpay.js
    updated acc.

--- Configuration/Razorpay.js
+++ Configuration/Razorpay.js
@@ -1,6 +1,10 @@
 const Razorpay = require("razorpay");
 
-exports.instance = new Razorpay({
-	key_id: process.env.RAZORPAY_KEY,
-	key_secret: process.env.RAZORPAY_SECRET,
-});
+try {
+    exports.instance = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY,
+        key_secret: process.env.RAZORPAY_SECRET,
+    });
+} catch (error) {
+    console.error("Error initializing Razorpay:", error);
+}
