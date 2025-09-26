e9268280eb3aca2e5070652a29e99bc4a4f8fa5a
Karan gupta <gupta.karan1.gh@gmail.com>
2025-09-26 11:50:38 +0530

    docker image1

--- Configuration/Database.js
+++ Configuration/Database.js
@@ -6,13 +6,13 @@ const { MONGODB_URL } = process.env;
 exports.connect = () => {
 	mongoose
 		.connect(MONGODB_URL, {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
+			// useNewUrlparser: true,
+			// useUnifiedTopology: true,
 		})
 		.then(console.log(`DB Connection Success`))
 		.catch((err) => {
-			// console.log(`DB Connection Failed`);
-			// console.log(err);
+			console.log(`DB Connection Failed`);
+			console.log(err);
 			process.exit(1);
 		});
 };
